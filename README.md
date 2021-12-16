b99ce0af46e2b0e19baf0c2110d49a8c84f11056
Vazgen005 <norvardyanvazgen@gmail.com>
2021-12-16 18:50:06 +0300

    add files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Paper-sami-auto-updater
+How to run
+
+git clone https://github.com/Vazgen005/Paper-sami-auto-updater
+
+pip install -r requirements.txt
+
+python main.py
---
+++ main.py
@@ -0,0 +1,37 @@
+import requests
+import json
+import os
+
+mcv = '1.18.1'
+
+print('Searching for a new version of paper...')
+
+main = json.loads(requests.get(url=f'https://papermc.io/api/v2/projects/paper/versions/{mcv}/').content)
+
+maxb = max(main['builds'])
+newv = json.loads(requests.get(url=f'https://papermc.io/api/v2/projects/paper/versions/{mcv}/builds/{maxb}').content)
+fname = newv['downloads']['application']['name']
+dw = f'https://papermc.io/api/v2/projects/paper/versions/{mcv}/builds/{maxb}/downloads/{fname}'
+
+print('New paper version was found!')
+
+if os.path.exists(f'{os.getcwd()}/{fname}'):
+    print('The latest version already exists')
+    os.system('pause')
+    exit()
+
+iterr = 0
+fold = os.listdir(os.getcwd())
+for i in fold:
+    if i.startswith(f'paper-{mcv}-') and i.endswith('.jar'):
+        print('Deleting the old version...')
+        os.remove(f'{os.getcwd()}/{fold[iterr]}')
+        break
+    iterr += 1
+
+print('Downloading new version...')
+
+with open(f'{os.getcwd()}/{fname}', 'wb') as f:
+    f.write(requests.get(url=dw).content)
+
+print(f'P{fname[1:]} successfully downloaded!')
